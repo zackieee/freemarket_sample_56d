70952017ef589bb7e7b771281c6b5576fddb336b
zackieee <zackie.jackie.222@gmail.com>
2019-08-07 10:30:27 +0900

    ルーティング設定
    ページ遷移のためにルーティングを設定しました。
    本ファイルはコンフリクトするため、チーム内でマージが必要です。
    
    （deviseコントローラのカスタムアクション作成と一緒に送ろうとしていたものです）

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+  devise_for :users, :controllers => {
+    :registrations => "users/registrations"
+  }
+
+  devise_scope :user do
+    get '/users/sign_up/profile' => 'users/registrations#new_profile'
+    get '/users/sign_up/telephone' => 'users/registrations#new_telephone'
+    get '/users/sign_up/telephone/auth' => 'users/registrations#new_telephone_auth'
+    get '/users/sign_up/address' => 'users/registrations#new_address' 
+    get '/users/sign_up/payment' => 'users/registrations#new_payment'
+    get '/users/sign_up/complete' => 'users/registrations#new_complete'
+  end
+
   root 'products#index'
   resources :products
   resources :users
