1164249537679f1aee1a9dcc71d8f6befb92793b
kunotomohiro <kunotomohiro215@gmail.com>
2019-08-08 12:41:07 +0900

    Create sessions_controller.rb

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,16 @@
+class Users::SessionsController < Devise::SessionsController
+  def edit_profile
+  end
+
+  def edit_address
+  end
+
+  def edit_payment
+  end
+
+  def edit_account
+  end
+
+  def sign_out
+  end
+end
