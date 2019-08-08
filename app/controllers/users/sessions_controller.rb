bf40d8ec91576e855dcea61574757e8b2d4051f1
kunotomohiro <kunotomohiro215@gmail.com>
2019-08-08 12:44:42 +0900

    Update sessions_controller.rb

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,16 +1,2 @@
 class Users::SessionsController < Devise::SessionsController
-  def edit_profile
-  end
-
-  def edit_address
-  end
-
-  def edit_payment
-  end
-
-  def edit_account
-  end
-
-  def sign_out
-  end
 end
