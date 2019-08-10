68f6f626fa14f0ad0b185cd5bdfc29dec66774fc
Katochan0927 <bz.yakyubaka.yk@gmail.com>
2019-08-10 12:05:37 +0900

    DB 修正

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :seller, class_name: 'User', foreign_key: 'seller_id'
-  belongs_to :buyer, class_name: 'User', foreign_key: 'buyer_id'
+  belongs_to :buyer, class_name: 'User', foreign_key: 'buyer_id', optional: true
   has_many :images, dependent: :destroy
   has_many :comments
   has_many :favorites
