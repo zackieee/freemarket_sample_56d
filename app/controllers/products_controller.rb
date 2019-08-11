b2264caf9e09bcdeb5494611cecb839a08750273
manbow-42 <yuukisasaki42@icloud.com>
2019-08-11 18:41:41 +0900

    パスの変更

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,7 +10,7 @@ class ProductsController < ApplicationController
   def create
     @product= Product.new(product_params)
     if @product.save
-      redirect_to root_path
+      redirect_to "/products"
     else
       redirect_to new_product_path
     end
