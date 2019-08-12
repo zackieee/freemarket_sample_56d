a949d15b9cbdbfe7ba4160290de35cbb538235f4
Katochan0927 <bz.yakyubaka.yk@gmail.com>
2019-08-12 11:17:47 +0900

    DBの調整

--- db/migrate/20190808102906_create_brands.rb
+++ db/migrate/20190808102906_create_brands.rb
@@ -1,7 +1,8 @@
 class CreateBrands < ActiveRecord::Migration[5.2]
   def change
     create_table :brands do |t|
-      t.string      :name,        null: false, index: true
+      t.string      :name,                  null: false, index: true
+      # t.string      :brand_category,        null: false
 
       t.timestamps
     end
