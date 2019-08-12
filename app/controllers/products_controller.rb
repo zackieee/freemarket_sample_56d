936a4b318e534314c8be4a951bb784e46e14d61b
Katochan0927 <bz.yakyubaka.yk@gmail.com>
2019-08-12 09:26:44 +0900

    before_action　使用

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,7 @@
 class ProductsController < ApplicationController
+
+  before_action :set_product, only: [:show, :buy]
+
   def index
     @products= Product.order('id DESC').limit(4)
   end
@@ -17,17 +20,19 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find(params[:id])
   end
 
   def buy
-    @product = Product.find(params[:id])
   end
   
   private
    def product_params
     params.require(:product).permit(:name, :text, :price, :status_id, :prefecture_id, :postage_burden_id, :buyer_area_id, :delivery_days_id, images: [])
    end
+
+   def set_product
+    @product = Product.find(params[:id])
+  end
 end
 
 # image: product_params[:image], name: product_params[:name], 
