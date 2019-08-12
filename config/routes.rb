114cd165ed3a0376282a5370d81128c6aefcc7be
manbow-42 <yuukisasaki42@icloud.com>
2019-08-12 16:49:38 +0900

    ルーティングとコントローラーの設定

--- config/routes.rb
+++ config/routes.rb
@@ -25,7 +25,7 @@ Rails.application.routes.draw do
 
   root 'products#index'
   resources :products do
-    get :buy, on: :collection #productsにデータを入れたら':collection'を':mender'に変更
+    get :buy, :selling_index, on: :collection #productsにデータを入れたら':collection'を':mender'に変更
   end
   resources :users
 end
