5bc1d4b6cf6514a871581057da720012455f7cef
Katochan0927 <bz.yakyubaka.yk@gmail.com>
2019-08-12 17:09:20 +0900

    buyer→user

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -2,7 +2,7 @@ class TradesController < ApplicationController
 
   def create
     product = Product.find(trade_params[:product_id])
-    trade = Trade.new(product_id: product[:id], seller_id: product[:seller_id], buyer_id: 2)  #ログイン機能ができたら、'current_user.id'に変更
+    trade = Trade.new(product_id: product[:id], seller_id: product[:seller_id], buyer_id: current_user.id)  #ログイン機能ができたら、'current_user.id'に変更
     
     # Payjp.api_key = 'Payjp の画面から取得した秘密鍵'
     # Payjp::Charge.create(
