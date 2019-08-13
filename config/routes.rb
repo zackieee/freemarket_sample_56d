47a59bc93ea593550b9d36cc524a06670436fa3c
manbow-42 <yuukisasaki42@icloud.com>
2019-08-13 11:27:48 +0900

    ルーティングにid付与

--- config/routes.rb
+++ config/routes.rb
@@ -27,8 +27,8 @@ Rails.application.routes.draw do
   root 'products#index'
   get 'products/selling_index' => 'products#selling_index'
   resources :products do
-    get 'products/selling_show' => 'products#selling_show'
-    get :buy #productsにデータを入れたら':collection'を':mender'に変更
+    get :selling_show, on: :member
+    get :buy, on: :member #productsにデータを入れたら':collection'を':mender'に変更
     resources :trades, only: [:create]
   end
   resources :users
