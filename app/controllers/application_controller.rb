4fd16a0b1799704b348bfbcb76bbbb6e9d947a33
kunotomohiro <kunotomohiro215@gmail.com>
2019-08-14 18:33:17 +0900

    ログアウトしたらindexに飛ばない問題解決
    ログアウトしたらindexに飛ばない問題解決

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
 
   def after_sign_in_path_for(resource)
     root_path
