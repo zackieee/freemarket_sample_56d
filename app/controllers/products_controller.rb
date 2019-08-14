fa49986c1988d0d58afe526613308eb310bc808b
Katochan0927 <bz.yakyubaka.yk@gmail.com>
2019-08-14 16:36:05 +0900

    Update products_controller.rb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -88,7 +88,7 @@ class ProductsController < ApplicationController
   
   private
    def product_params
-    params.require(:product).permit(:name, :text, :price, :status_id, :prefecture_id, :postage_burden_id, :delivery_days_id, images: [] ).merge(seller_id: current_user)
+    params.require(:product).permit(:name, :text, :price, :status_id, :prefecture_id, :postage_burden_id, :delivery_days_id, images: [] ).merge(seller_id: current_user.id)
    end
 
    def set_product
