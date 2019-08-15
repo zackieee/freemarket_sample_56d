84e058701a9b17215fbcad832f3affcdfc89ad3d
zackieee <zackie.jackie.222@gmail.com>
2019-08-15 09:31:39 +0900

    payjpのjsに必須チェック追加
    例外処理は未適用（多分ここ必須）

--- app/assets/javascripts/payjp.js
+++ app/assets/javascripts/payjp.js
@@ -2,8 +2,45 @@ document.addEventListener(
   "DOMContentLoaded", (e) => {
     Payjp.setPublicKey("pk_test_fce09d900358cad3e1310579");
     const btn = document.getElementById('token_submit');
+
+    const check_list_number = $("#card-number-check-result");
+    function appendCheckNumberResult(){
+      var html =
+        `<div class="error-messages">
+            <ul>
+              <li>必須項目です</li>
+            </ul>
+          </div>`
+      check_list_number.append(html);
+    }
+  
+    const check_list_cvc = $("#security-code-check-result");
+    function appendCheckCVCResult(){
+      var html =
+        `<ul class="error-messages">
+            <li>必須項目です</li>
+          </ul>`
+      check_list_cvc.append(html);
+    }
+  
+    function checkForm(){
+      var flag = 0;
+      if(document.getElementById("card_number").value == ""){
+        flag = 1;
+        appendCheckNumberResult();
+      }
+      if(document.getElementById("card_security_code").value == ""){
+        flag = 1;
+        appendCheckCVCResult();
+      }
+      return flag;
+    }
+  
     btn.addEventListener("click", (e) => {
       e.preventDefault();
+
+      var result = checkForm();
+      if(result != 1){
 
       //カード情報生成
       //画面から情報を取得します
@@ -13,7 +50,6 @@ document.addEventListener(
         exp_month: document.getElementById("_expiration_date_2i").value,
         exp_year: 20 + document.getElementById("_expiration_date_1i").value
       }
-
       //トークン生成
       //カード情報はサーバ通信に含めないように削除する
       Payjp.createToken(card, (status, response) => {
@@ -27,7 +63,10 @@ document.addEventListener(
           ); //トークンを送信できるように隠しタグを生成（これが走ってコントローラに飛ぶ)
           document.inputForm.submit();
         } else {
+          //TODO:Payjp.createTokenの例外処理はここに記述。
+          //try/chatchにする場合は構造自体に変更ある可能せ合いあり
         }
       });
-    });
-  },false);
+    }
+  });
+},false);
