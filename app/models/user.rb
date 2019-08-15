ef685683321bf88ffdb93fd9d967ecde8302ef39
zackieee <zackie.jackie.222@gmail.com>
2019-08-15 09:24:53 +0900

    バリデーションチェック追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  include ActiveRecord::Reflection
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -13,4 +15,87 @@ class User < ApplicationRecord
   has_many :favorites
   has_many :rated_user_ratings, class_name: 'Rating', foreign_key: 'rated_user_id'
   has_many :rater_user_ratings, class_name: 'Rating', foreign_key: 'rater_user_id'
+
+  # fields_for用のネスト設定
+  accepts_nested_attributes_for :address, allow_destroy: true
+
+  # 会員情報登録画面用のバリデーション
+  with_options on: :profile do | profile |
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+    profile.validates :nickname,
+      presence: true,
+      length:{ maximum: 20}
+    profile.validates :email,
+      presence: true,
+      format: { with: VALID_EMAIL_REGEX , message: 'のフォーマットが不適切です'}
+    profile.validates :password,
+      presence:true,
+      confirmation: true,
+      length:{ minimum: 7, maximum: 128},
+      format: { with: VALID_PASSWORD_REGEX , message: 'は英字と数字両方を含むパスワードを設定してください'}
+    profile.validates :password_confirmation,
+      presence: true,
+      length:{ minimum: 7, maximum: 128},
+      format: { with: VALID_PASSWORD_REGEX , message: 'は英字と数字両方を含むパスワードを設定してください'}
+    profile.validates :lastname,
+      presence: true,
+      length:{ maximum: 35}
+    profile.validates :firstname,
+      presence: true,
+      length:{ maximum: 35}
+    profile.validates :lastname_kana,
+      presence: true,
+      length:{ maximum: 35},
+      format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい' }
+    profile.validates :firstname_kana,
+      presence: true,
+      length: { maximum: 35},
+      format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい' }
+    profile.validate :check_birthday  #生年月日用の独自バリデーション
+  end
+
+  # 電話番号確認画面用のバリデーション
+  with_options on: :tel do | tel |
+    tel.validates :tel,
+      presence: true,
+      format: { with: /\A\d{10,11}\z/, message: 'の入力が正しくありません'}
+  end
+
+  # 発送元・お届け先住所入力画面用のバリデーション(Addressモデル分も含む)
+  with_options on: :address do | address |
+    VALID_PHONE_REGEX = /\A\d{10}$|^\d{11}\z/
+    address.validates :lastname,
+      presence: true,
+      length:{ maximum: 35}
+    address.validates :firstname,
+      presence: true,
+      length:{ maximum: 35}
+    address.validates :lastname_kana,
+      presence: true,
+      length:{ maximum: 35},
+      format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい' }
+    address.validates :firstname_kana,
+      presence: true,
+      length: { maximum: 35},
+      format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい' }
+    address.validates :tel,
+      presence: true,
+      format: { with: VALID_PHONE_REGEX, message: '電話番号が正しくありません' }
+  end
+
+  # 生年月日のチェックメソッド
+  def check_birthday
+    # 生年月日が入力済かつ未来日(現在日付より未来)
+    if birthday.present?
+      if birthday > Date.today
+        # 未来日付の場合、エラー表示
+        errors.add(:birthday, "を正しく入力してください")
+      end
+    else
+      # 未入力の場合、エラー表示
+      errors.add(:birthday, "を入力してください")
+    end
+  end
+
 end
