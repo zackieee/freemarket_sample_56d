3251910aac0020dca5b8bfdc1dc77ab2270f9e91
kunotomohiro <kunotomohiro215@gmail.com>
2019-08-15 17:58:46 +0900

    ログインせずに購入画面に進もうとするとログイン画面に飛ばすよう変更
    ログインせずに購入画面に進もうとするとログイン画面に飛ばすよう変更。

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,7 +3,7 @@ class ProductsController < ApplicationController
   require 'payjp'
 
   before_action :set_product, only: [:show, :buy, :selling_show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!, only: [:buy]
   def index
     @products= Product.where.not(seller_id: current_user&.id).order('id DESC').limit(4)
   end
