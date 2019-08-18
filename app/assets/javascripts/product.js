aeb2c8fbb705ed89ecc67dd6f6dc8a1ae96ead9d
kunotomohiro <kunotomohiro215@gmail.com>
2019-08-18 13:25:36 +0900

    必要のない記述の削除とパンくずの文字を微調整
    必要のない記述の削除とパンくずの文字を微調整

--- app/assets/javascripts/product.js
+++ app/assets/javascripts/product.js
@@ -1,25 +1,3 @@
-$(function(){
-  var menu = $('.aaaa'), // スライドインするメニューを指定
-      menuBtn = $('arow'), // メニューボタンを指定
-      body = $(document.body),    
-      menuWidth = menu.outerWidth();                
-       
-      // メニューボタンをクリックした時の動き
-      menuBtn.on('click', function(){
-      // body に open クラスを付与する
-      body.toggleClass('open');
-          if(body.hasClass('open')){
-              // open クラスが body についていたらメニューをスライドインする
-              body.animate({'left' : menuWidth }, 300);            
-              menu.animate({'left' : 0 }, 300);                    
-          } else {
-              // open クラスが body についていなかったらスライドアウトする
-              menu.animate({'left' : -menuWidth }, 300);
-              body.animate({'left' : 0 }, 300);            
-          }             
-      });
-  });    
-
 $(function() {
 
   function buildHTML(url){
