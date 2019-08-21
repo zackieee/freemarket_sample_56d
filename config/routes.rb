f706115bd5f831cf606fe39ade49621e4cc1bcda
zackieee <zackie.jackie.222@gmail.com>
2019-08-21 11:56:50 +0900

    通知機能用＋メール用のルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,35 +5,39 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
    }
 
+  # 開発環境で擬似メール送信結果を見るためのルーティング
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+
   devise_scope :user do
     #ユーザ新規作成
-    get '/users/sign_up/profile' => 'users/registrations#new_profile'
-    get '/users/sign_up/telephone' => 'users/registrations#new_telephone'
-    get '/users/sign_up/telephone/auth' => 'users/registrations#new_telephone_auth'
-    get '/users/sign_up/telephone/check' => 'users/registrations#authcode_check'
-    get '/users/sign_up/address' => 'users/registrations#new_address' 
-    get '/users/sign_up/payment' => 'users/registrations#new_payment'
-    post '/users/sign_up/complete' => 'users/registrations#new_complete'
-    post 'users/omniauth_callbacks' => 'users/omniauth_callbacks#passthru'
+    get     '/users/sign_up/profile'        => 'users/registrations#new_profile'
+    get     '/users/sign_up/telephone'      => 'users/registrations#new_telephone'
+    get     '/users/sign_up/telephone/auth' => 'users/registrations#new_telephone_auth'
+    get     '/users/sign_up/telephone/check'=> 'users/registrations#authcode_check'
+    get     '/users/sign_up/address'        => 'users/registrations#new_address' 
+    get     '/users/sign_up/payment'        => 'users/registrations#new_payment'
+    post    '/users/sign_up/complete'       => 'users/registrations#new_complete'
+    post    'users/omniauth_callbacks'      => 'users/omniauth_callbacks#passthru'
     #ユーザ編集
-    get '/users/edit/profile' => 'users/registrations#edit_profile'
-    post '/users/edit/profile' => 'users/registrations#update_profile'
-    get '/users/edit/address' => 'users/registrations#edit_address' 
-    post '/users/edit/address' => 'users/registrations#update_address' 
-    get '/users/edit/payment' => 'users/registrations#edit_payment'
-    get '/users/edit/payment_2' => 'users/registrations#edit_payment_2'
-    post '/users/edit/payment_2' => 'users/registrations#update_payment'
-    delete '/user/delete/payment' => 'users/registrations#destroy_payment'
-    get '/users/edit/account' => 'users/registrations#edit_account'
-    post '/users/edit/account' => 'users/registrations#update_account'
-    get '/users/edit/telephone' => 'users/registrations#edit_telephone'
-    get '/users/edit/telephone/auth' => 'users/registrations#edit_telephone_auth'
-    post '/users/edit/telephone/auth' => 'users/registrations#update_telephone_auth'
+    get     '/users/edit/profile'           => 'users/registrations#edit_profile'
+    post    '/users/edit/profile'           => 'users/registrations#update_profile'
+    get     '/users/edit/address'           => 'users/registrations#edit_address' 
+    post    '/users/edit/address'           => 'users/registrations#update_address' 
+    get     '/users/edit/payment'           => 'users/registrations#edit_payment'
+    get     '/users/edit/payment_2'         => 'users/registrations#edit_payment_2'
+    post    '/users/edit/payment_2'         => 'users/registrations#update_payment'
+    delete  '/user/delete/payment'          => 'users/registrations#destroy_payment'
+    get     '/users/edit/account'           => 'users/registrations#edit_account'
+    post    '/users/edit/account'           => 'users/registrations#update_account'
+    get     '/users/edit/telephone'         => 'users/registrations#edit_telephone'
+    get     '/users/edit/telephone/auth'    => 'users/registrations#edit_telephone_auth'
+    post    '/users/edit/telephone/auth'    => 'users/registrations#update_telephone_auth'
     # パスワード編集
-    get 'users/edit/password' => 'users/registrations#edit_password'
-    post 'users/edit/password' => 'users/registrations#update_password'
+    get     'users/edit/password'           => 'users/registrations#edit_password'
+    post    'users/edit/password'           => 'users/registrations#update_password'
+    get     'users/edit/password_2'         => 'users/registrations#edit_password_2'
     #ユーザサインアウト画面
-    get '/users/sign_out' => 'users/sessions#sign_out_confirm'
+    get     '/users/sign_out'               => 'users/sessions#sign_out_confirm'
   end
 
   root 'products#index'
@@ -44,11 +48,19 @@ Rails.application.routes.draw do
   resources :products do
     get :selling_show, on: :member
     get :buy, on: :member #productsにデータを入れたら':collection'を':mender'に変更
-    resources :trades, only: [:create]
+    resources :trades, only: [:create, :show ,:edit, :update]
   end
   resources :users do
     member do
       get 'show_profile'
     end
   end
+
+  get 'notifications/index_todo'  =>  'notifications#index_todo'
+  resources :notifications do
+    member do
+      get 'dispatcher'
+    end
+  end
+
 end
