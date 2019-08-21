5ea00320fc0c4e7827947d1c256c7feec77e3864
zackieee <zackie.jackie.222@gmail.com>
2019-08-21 12:11:45 +0900

    payjp.jsエラーがconsoleに出る問題を修正

--- app/assets/javascripts/payjp.js
+++ app/assets/javascripts/payjp.js
@@ -35,38 +35,43 @@ document.addEventListener(
       }
       return flag;
     }
-  
-    btn.addEventListener("click", (e) => {
-      e.preventDefault();
 
-      var result = checkForm();
-      if(result != 1){
+    if (btn == null){
+      // btnがnullなら何もしない
+    }else{
+      btn.addEventListener("click", (e) => {
 
-      //カード情報生成
-      //画面から情報を取得します
-      const card = {
-        number: document.getElementById("card_number").value,
-        cvc: document.getElementById("card_security_code").value,
-        exp_month: document.getElementById("_expiration_date_2i").value,
-        exp_year: 20 + document.getElementById("_expiration_date_1i").value
-      }
-      //トークン生成
-      //カード情報はサーバ通信に含めないように削除する
-      Payjp.createToken(card, (status, response) => {
-        if (status === 200) { //成功した場合
-          $("#card_number").removeAttr("name");
-          $("#card_security_code").removeAttr("name");
-          $("#_expiration_date_2i").removeAttr("name");
-          $("#_expiration_date_1i").removeAttr("name");
-          $("#card_token").append(
-            $('<input type="hidden" name="payjp_token">').val(response.id)
-          ); //トークンを送信できるように隠しタグを生成（これが走ってコントローラに飛ぶ)
-          document.inputForm.submit();
-        } else {
-          //TODO:Payjp.createTokenの例外処理はここに記述。
-          //try/chatchにする場合は構造自体に変更ある可能せ合いあり
+        e.preventDefault();
+
+        var result = checkForm();
+        if(result != 1){
+
+        //カード情報生成
+        //画面から情報を取得します
+        const card = {
+          number: document.getElementById("card_number").value,
+          cvc: document.getElementById("card_security_code").value,
+          exp_month: document.getElementById("_expiration_date_2i").value,
+          exp_year: 20 + document.getElementById("_expiration_date_1i").value
         }
-      });
-    }
-  });
+        //トークン生成
+        //カード情報はサーバ通信に含めないように削除する
+        Payjp.createToken(card, (status, response) => {
+          if (status === 200) { //成功した場合
+            $("#card_number").removeAttr("name");
+            $("#card_security_code").removeAttr("name");
+            $("#_expiration_date_2i").removeAttr("name");
+            $("#_expiration_date_1i").removeAttr("name");
+            $("#card_token").append(
+              $('<input type="hidden" name="payjp_token">').val(response.id)
+            ); //トークンを送信できるように隠しタグを生成（これが走ってコントローラに飛ぶ)
+            document.inputForm.submit();
+          } else {
+            //TODO:Payjp.createTokenの例外処理はここに記述。
+            //try/chatchにする場合は構造自体に変更ある可能せ合いあり
+          }
+        });
+      }
+    });
+  }
 },false);
