f7ffebc7631b2f400e470aa5064b29499a6e4432
zackieee <zackie.jackie.222@gmail.com>
2019-08-21 12:09:34 +0900

    ユーザ評価・出品数取得、表示機能改修

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,30 @@
 class UsersController < ApplicationController
+  before_action :notice_count, only: [:show]
+  before_action :todo_count,   only: [:show]
+
   def show
+    # 評価／出品数を取得
+    @rating_count   = Rating.where(rated_user_id: current_user.id).count
+    @trade_count    = Trade.where(seller_id: current_user.id).count
+
+    # お知らせ情報取得
+    @notifications  = Notification.where.not(action: 5 ).where(receiver_id: current_user.id).order("created_at DESC")
+
+    # 取引中の商品取得
+    @trade_products = Trade.where.not(trade_status: 3).where("(seller_id = ?) OR (buyer_id = ?)",current_user.id,current_user.id).order("created_at DESC")
+
   end
 
   # これを呼ぶと見たいユーザのプロフィールページが参照できる
   def show_profile
     @user = User.find(params[:id])
+
+    @notifications  =  Notification.where.not(action: 5 ).where(receiver_id: @user.id).order("created_at DESC")
+    @rating_count   =  Rating.where(rated_user_id: @user.id).count
+    @raiting_good   =  Rating.where(rated_user_id: @user.id, rate: 1).count
+    @raiting_normal =  Rating.where(rated_user_id: @user.id, rate: 2).count
+    @raiting_bad    =  Rating.where(rated_user_id: @user.id, rate: 3).count
+    @trade_count    =  Trade.where(seller_id: @user.id).count
+
   end
 end
