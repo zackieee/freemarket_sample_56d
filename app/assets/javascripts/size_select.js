6bf3c6842c6928c3bf2eaeaa73149c3ba51a857e
Katochan0927 <bz.yakyubaka.yk@gmail.com>
2019-08-22 11:16:56 +0900

    デバック削除

--- app/assets/javascripts/size_select.js
+++ app/assets/javascripts/size_select.js
@@ -20,7 +20,6 @@ $(function(){
   $(document).on('change','#category_box_2', function(){
     $('#size_box').empty();
     var category_id = Number(this.value);
-    console.log(category_id);
     const size_category_1 = [158,159,160,161,162,163,164,165,166,167,168,168,169,170,
                              171,172,173,174,175,176,177,178,179,180,181,182,183,184,
                              185,186,187,188,189,190,191,192,193,194,195,196,197,198,
