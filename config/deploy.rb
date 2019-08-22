5115ef0d9f6b15a40fa7feeebf6824cfbd4ea659
manbow-42 <yuukisasaki42@icloud.com>
2019-08-22 16:38:16 +0900

    changing setting

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,10 +35,8 @@ namespace :deploy do
   desc 'db_seed'
   task :db_seed do
     on roles(:db) do |host|
-      with rails_env: fetch(:rails_env) do
-        within current_path do
-          execute :bundle, :exec, :rake, 'db:seed'
-        end
+      within current_path do
+        execute :bundle, :exec, :rake, 'db:seed'
       end
     end
   end
