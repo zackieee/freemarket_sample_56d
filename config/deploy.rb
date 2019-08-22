ddafd3df223300e565b9d176b5dba7f3a4711cdc
manbow-42 <yuukisasaki42@icloud.com>
2019-08-22 16:44:55 +0900

    delete setting

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,19 +30,8 @@ set :keep_releases, 5
 
 # デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
-SSHKit.config.command_map[:rake] = 'bundle exec rake'
 
 namespace :deploy do
-  desc 'db_seed'
-  task :db_seed do
-    on roles(:db) do |host|
-      with rails_env: fetch(:rails_env) do
-        within current_path do
-          execute :rake, 'db:seed'
-        end
-      end
-    end
-  end
   task :restart do
     invoke 'unicorn:restart'
   end
